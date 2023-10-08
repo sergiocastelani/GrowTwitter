b2b641561fdff2905192426f72cf674c84b03d07
Sergio C Castelani Jr <sergio.castelani@gmail.com>
2023-10-08 18:16:02 -0300

    Putting some initial info for tests

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,15 @@ import { View } from "./View";
 
 //----
 //fill DB
-UserController.createUser('Sergio', 'sergio', 'sergio@gmail.com', '1234');
-UserController.createUser('João', 'joao', 'joao@gmail.com', '1234');
+UserController.createUser('Sergio Castelani', 'sergio', 'sergio@gmail.com', '1234');
+UserController.createUser('João Silva', 'joao', 'joao@gmail.com', '1234');
+
+let tweet = TweetController.createTweet('sergio', 'Hello World!');
+TweetController.like('joao', tweet.id);
+TweetController.reply('joao', tweet.id, 'Hi there!');
+
+TweetController.createTweet('sergio', 'Hello again!');
+TweetController.like('joao', tweet.id);
 
 //----
 //main loop
