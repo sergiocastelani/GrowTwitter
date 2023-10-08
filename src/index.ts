863c95071741d1248db8f00e902fbf594d734d10
Sergio C Castelani Jr <sergio.castelani@gmail.com>
2023-10-08 18:24:10 -0300

    more initial data

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,21 @@ import { View } from "./View";
 //fill DB
 UserController.createUser('Sergio Castelani', 'sergio', 'sergio@gmail.com', '1234');
 UserController.createUser('João Silva', 'joao', 'joao@gmail.com', '1234');
+UserController.createUser('Pedro Paulo', 'pedro', 'pedro@gmail.com', '1234');
 
 let tweet = TweetController.createTweet('sergio', 'Hello World!');
 TweetController.like('joao', tweet.id);
+TweetController.like('pedro', tweet.id);
 TweetController.reply('joao', tweet.id, 'Hi there!');
+TweetController.reply('joao', tweet.id, 'I liked this one!');
 
-TweetController.createTweet('sergio', 'Hello again!');
+tweet = TweetController.createTweet('sergio', 'Hello again!');
 TweetController.like('joao', tweet.id);
+TweetController.like('pedro', tweet.id);
+
+tweet = TweetController.createTweet('sergio', "I'm alive!!!");
+TweetController.like('joao', tweet.id);
+TweetController.reply('pedro', tweet.id, 'Get a live');
 
 //----
 //main loop
